76552ab436d88e386e4efe335609527c9f8310eb
AlGD09 <alejandro.durrer@gmail.com>
2025-10-15 14:24:38 +0200

    Advertising Nachricht "Hallo"

--- main.py
+++ main.py
@@ -1,64 +1,53 @@
 #!/usr/bin/env python3
-# main.py - BLE Advertising über BlueZ-DBus (RCU-kompatibel)
+# main.py – BLE Advertising mit kurzer Nachricht
 
 import asyncio
+import os
+from dbus_fast import BusType, Variant
 from dbus_fast.aio import MessageBus
-from dbus_fast.service import ServiceInterface, method, dbus_property, PropertyAccess
-from dbus_fast import BusType
-from dbus_fast import Variant
-
-ADAPTER_IFACE = 'org.bluez.Adapter1'
-LE_ADV_MGR_IFACE = 'org.bluez.LEAdvertisingManager1'
-LE_ADV_IFACE = 'org.bluez.LEAdvertisement1'
-BLUEZ_SERVICE_NAME = 'org.bluez'
-ADVERT_PATH = '/org/bluez/example/advertisement0'
-
+from dbus_fast.service import ServiceInterface, method
 
 class Advertisement(ServiceInterface):
     def __init__(self, bus, path):
-        super().__init__(LE_ADV_IFACE)
+        super().__init__('org.bluez.LEAdvertisement1')
         self.path = path
-        self.bus = bus
-        self.properties = {
-            "Type": Variant("s", "peripheral"),
-            "LocalName": Variant("s", "RCU_Test"),
-            "Discoverable": Variant("b", True),
-            "Includes": Variant("as", ["tx-power"]),
+        self.props = {
+            'Type': Variant('s', 'peripheral'),
+            'LocalName': Variant('s', 'RCU_Test'),
+            'ServiceData': Variant('a{sv}', {
+                '9999': Variant('ay', b'Hallo')  # kurze Nachricht im ServiceData-Feld
+            }),
+            'IncludeTxPower': Variant('b', True),
         }
+
+    def get_path(self):
+        return self.path
 
     @method()
     def Release(self):
         print("Advertisement released")
 
-    @dbus_property(access=PropertyAccess.READ)
-    def Type(self) -> 's':
-        return self.properties["Type"].value
-
-
 async def main():
+    os.system("bluetoothctl power on")
+    os.system("bluetoothctl system-alias RCU_Test")
     bus = await MessageBus(bus_type=BusType.SYSTEM).connect()
 
-    obj = await bus.introspect(BLUEZ_SERVICE_NAME, "/org/bluez/hci0")
-    adapter = bus.get_proxy_object(BLUEZ_SERVICE_NAME, "/org/bluez/hci0", obj)
-    adv_manager = adapter.get_interface(LE_ADV_MGR_IFACE)
+    ad_manager = await bus.introspect('org.bluez', '/org/bluez/hci0')
+    ad_manager_obj = bus.get_proxy_object('org.bluez', '/org/bluez/hci0', ad_manager)
+    ad_manager_iface = ad_manager_obj.get_interface('org.bluez.LEAdvertisingManager1')
 
-    advertisement = Advertisement(bus, ADVERT_PATH)
-    bus.export(ADVERT_PATH, advertisement)
+    ad = Advertisement(bus, '/org/bluez/example/advertisement0')
+    bus.export('/org/bluez/example/advertisement0', ad)
+
+    await ad_manager_iface.call_register_advertisement('/org/bluez/example/advertisement0', {})
+    print("🔵 Advertising gestartet mit Nachricht 'Hallo' (Name: RCU_Test)")
 
     try:
-        await adv_manager.call_register_advertisement(ADVERT_PATH, {})
-        print("Advertising gestartet als 'RCU_Test' ... Stop mit Ctrl+C")
         while True:
-            await asyncio.sleep(2)
-    except Exception as e:
-        print("Fehler:", e)
-    finally:
-        try:
-            await adv_manager.call_unregister_advertisement(ADVERT_PATH)
-        except Exception:
-            pass
-        print("Advertising gestoppt")
-
+            await asyncio.sleep(1)
+    except KeyboardInterrupt:
+        await ad_manager_iface.call_unregister_advertisement('/org/bluez/example/advertisement0')
+        print("🛑 Advertising beendet")
 
 if __name__ == "__main__":
     asyncio.run(main())
