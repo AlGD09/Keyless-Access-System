b037440a449bc7946c186204c29a52a2b7672d9b
AlGD09 <alejandro.durrer@gmail.com>
2025-10-15 13:19:40 +0200

    Main Avertising versuchen -rein Blz

--- main.py
+++ main.py
@@ -1,16 +1,63 @@
 #!/usr/bin/env python3
-# main.py – Testprogramm für RCU-Funktionalität
+# main.py - BLE Advertising über BlueZ-DBus (RCU-kompatibel)
 
-from bleak import BleakAdvertiser
 import asyncio
+from dbus_fast.aio import MessageBus
+from dbus_fast.service import ServiceInterface, method, dbus_property, PropertyAccess
+from dbus_fast import Variant
+
+ADAPTER_IFACE = 'org.bluez.Adapter1'
+LE_ADV_MGR_IFACE = 'org.bluez.LEAdvertisingManager1'
+LE_ADV_IFACE = 'org.bluez.LEAdvertisement1'
+BLUEZ_SERVICE_NAME = 'org.bluez'
+ADVERT_PATH = '/org/bluez/example/advertisement0'
+
+
+class Advertisement(ServiceInterface):
+    def __init__(self, bus, path):
+        super().__init__(LE_ADV_IFACE)
+        self.path = path
+        self.bus = bus
+        self.properties = {
+            "Type": Variant("s", "peripheral"),
+            "LocalName": Variant("s", "RCU_Test"),
+            "Discoverable": Variant("b", True),
+            "Includes": Variant("as", ["tx-power"]),
+        }
+
+    @method()
+    def Release(self):
+        print("Advertisement released")
+
+    @dbus_property(access=PropertyAccess.READ)
+    def Type(self) -> 's':
+        return self.properties["Type"].value
+
 
 async def main():
-    async with BleakAdvertiser(advertisement_data={"local_name": "RCU_Test"}) as adv:
-        print("Advertising as RCU_Test... Press Ctrl+C to stop.")
+    bus = await MessageBus().connect()
+
+    obj = await bus.introspect(BLUEZ_SERVICE_NAME, "/org/bluez/hci0")
+    adapter = bus.get_proxy_object(BLUEZ_SERVICE_NAME, "/org/bluez/hci0", obj)
+    adv_manager = adapter.get_interface(LE_ADV_MGR_IFACE)
+
+    advertisement = Advertisement(bus, ADVERT_PATH)
+    bus.export(ADVERT_PATH, advertisement)
+
+    try:
+        await adv_manager.call_register_advertisement(ADVERT_PATH, {})
+        print("Advertising gestartet als 'RCU_Test' ... Stop mit Ctrl+C")
         while True:
-            await asyncio.sleep(1)
+            await asyncio.sleep(2)
+    except Exception as e:
+        print("Fehler:", e)
+    finally:
+        try:
+            await adv_manager.call_unregister_advertisement(ADVERT_PATH)
+        except Exception:
+            pass
+        print("Advertising gestoppt")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
